cacb67bcb2f1eee742ff199483fc38ff171e82bc
Miguel Cifuentes <miguelcifuentes@ustavillavo.edu.co>
2026-05-21 09:00:20 -0500

    Creacion de Proyecto

---
+++ src/test/java/com/example/goldenticket2/Goldenticket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goldenticket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Goldenticket2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
